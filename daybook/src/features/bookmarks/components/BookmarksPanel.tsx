cceb698483aee081e83e434cb6532f0d1cddd21b
Balša Bazović <balsa.bazovic@gmail.com>
2026-07-19 02:53:34 -0400

    Updated the BookmarksPanel component by removing the z-index property since its parent is already set to relative.

--- daybook/src/features/bookmarks/components/BookmarksPanel.tsx
+++ daybook/src/features/bookmarks/components/BookmarksPanel.tsx
@@ -11,7 +11,7 @@ export function BookmarksPanel() {
     const bookmarkEntries = Object.entries(bookmarks);
 
     return (
-        <div className="absolute top-12 -right-2 z-9998 w-80">
+        <div className="absolute top-12 -right-2 w-80">
             <Popover title="Saved" onClose={handleToggleBookmarksPanel}>
                 {bookmarkEntries.length === 0 ? (
                     <p className="py-4 px-4 text-sm text-left text-subtle">Nothing saved yet. Click the bookmark icon on any entry to have it appear here.</p>
